6f9b54a0e1cdf7751bb0778524f9ea955ca16157
PetterPet <62211291+PetterPet01@users.noreply.github.com>
2022-08-31 14:40:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install qemu-kvm qemu virt-manager virt-viewer libvirt-bin qemu-system qemu-utils python3 python3-pip git qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf
+sudo apt-get install qemu-kvm qemu virt-manager virt-viewer libvirt-daemon-system libvirt-clients qemu-system qemu-utils python3 python3-pip git qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager ovmf
 cd ~
 git clone https://github.com/downthecrop/macOS-Simple-KVM
 cd macOS-Simple-KVM
